43aa0f24d39e38464a7cc80c1d6899e6f308aacc
Daijya Robinson <QuestGem01@gmail.com>
2026-01-22 14:59:21 -0500

    Level Work
    Today I spent a lot of the time understanding titlemaps in Unity. There is now a grass background, a chest, and the character loads in. The next steep is to get the point and click function in game.

---
+++ The Noble Adventure/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*/// <summary>
+    /// Player Variables
+    /// </summary>
+
+    // Privtae
+
+    // Public
+    public Rigidbody2D playerRB;
+
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        Vector2 inputPos = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
+        Debug.Log(inputPos);
+    }*/
+}
--- The Noble Adventure/Assets/Scripts/Singletons/GameManger.cs
+++ The Noble Adventure/Assets/Scripts/Singletons/GameManger.cs
@@ -1,8 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManger : MonoBehaviour
+public class GameManger : SingletonManger<GameManger>
 {
+    /// <summary>
+    /// Used to transtion between scenes
+    /// </summary>
+    
     // Loads in the game scene
     public void loadMenuScene()
     {
@@ -26,5 +31,16 @@ public class GameManger : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("You have quit the game!");
+    }
+
+    /// <summary>
+    ///  Point and Click 
+    /// </summary>
+    /// 
+
+    void Update()
+    {
+        Vector2 inputPos = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
+        Debug.Log(inputPos);
     }
 }
