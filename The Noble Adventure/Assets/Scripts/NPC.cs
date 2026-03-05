3b060c18b2151359524a6242cbfb313a3baac070
Daijya Robinson <QuestGem01@gmail.com>
2026-03-05 15:29:52 -0500

    We have a level
    After a bit of bugs I was able to make our level!

--- The Noble Adventure/Assets/Scripts/NPC.cs
+++ The Noble Adventure/Assets/Scripts/NPC.cs
@@ -6,7 +6,7 @@ public class NPC : MonoBehaviour, IInteractable
     public bool IsShown { get; private set; }
     public string NPCId { get; private set; }
 
-
+    //public Canvas[] textBox;
     public Canvas textBox;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
